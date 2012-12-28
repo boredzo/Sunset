b7948ee87325d341c005e7824772a5323b23568e
Peter Hosey <git@boredzo.org>
2012-12-28 15:09:04 -0800

    Initial Commit

---
+++ Sunset/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Sunset
+//
+//  Created by Peter Hosey on 2012-12-28.
+//  Copyright (c) 2012 Peter Hosey. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
